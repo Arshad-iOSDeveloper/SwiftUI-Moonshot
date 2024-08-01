8e12ecd7cc24c7a9f9c1278882f45a3d120fbd0d
Arshad Shaik <arshad.shaik@livehealth.in>
2024-08-01 15:02:05 +0530

    First Commit

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Arshad Shaik on 22/07/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+  let id: String
+  let name: String
+  let description: String
+}
---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Arshad Shaik on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
